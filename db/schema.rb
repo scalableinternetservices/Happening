40aa8bcc92be1aef52e90e6aab7509de08fa52f5
Nicholas Ma <nicholashma@gmail.com>
2015-10-23 04:34:33 -0700

    Added Events scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016111013) do
+ActiveRecord::Schema.define(version: 20151016220210) do
+
+  create_table "events", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "time"
+    t.string   "location"
+    t.string   "owner"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
