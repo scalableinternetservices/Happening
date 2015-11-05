60cae5467d9533f58963c8678144f013b97f7cfe
Nicholas Ma <nicholashma@gmail.com>
2015-11-05 15:39:49 -0800

    A little HTML/CSS stuff

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  validates :name, :location, :description, :host, presence: true
-  validates :name, uniqueness: { case_sensitive: false }
   belongs_to :user
+  validates :user_id, :name, :location, :description, :host, presence: true
+  validates :name, uniqueness: { case_sensitive: false }
 end
