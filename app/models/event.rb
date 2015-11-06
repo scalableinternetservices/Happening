d20904b261bc1fc5e4420f97fbea9c135a13af7d
Nicholas Ma <nicholashma@gmail.com>
2015-11-05 16:40:12 -0800

    commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :user
-  validates :user_id, :name, :location, :description, :host, presence: true
+  validates :name, :location, :description, :host, presence: true
   validates :name, uniqueness: { case_sensitive: false }
 end
